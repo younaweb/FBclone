b1fa369c2666755e7245baf10907cfe48f204819
YoussefHarizi <youssef.harizi@gmail.com>
2022-01-20 12:38:17 +0100

    passport& ui auth vue &vuerouter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
+
 require('./bootstrap');
+import Vue from 'vue'
+import router from './router';
+import App from './components/App';
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+const app = new Vue({
+    el: '#app',
+    router,
+    components:{
+        App,
+    }
+});
